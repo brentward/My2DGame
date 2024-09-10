a925a2289bad1ee3e9ffd99633b81b19e8a1ea5f
Brent Bettis <brent.bettis@gmail.com>
2024-09-10 12:05:55 -0700

    Sprites and sprite animation

--- src/main/java/org/brentwardindustries/main/GamePanel.java
+++ src/main/java/org/brentwardindustries/main/GamePanel.java
@@ -1,5 +1,7 @@
 package org.brentwardindustries.main;
 
+import org.brentwardindustries.entity.Player;
+
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -11,7 +13,7 @@ public class GamePanel  extends JPanel implements Runnable {
     final int originalTileSize = 16;
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;
+    public final int tileSize = originalTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;
@@ -22,6 +24,7 @@ public class GamePanel  extends JPanel implements Runnable {
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player  = new Player(this, keyH);
 
     // Set player's default position
     int playerX = 100;
@@ -99,18 +102,7 @@ public class GamePanel  extends JPanel implements Runnable {
 //        }
 //    }
     public void update() {
-        if (keyH.upPressed) {
-            playerY -= playerSPeed;
-        }
-        if (keyH.downPressed) {
-            playerY += playerSPeed;
-        }
-        if (keyH.leftPressed) {
-            playerX -= playerSPeed;
-        }
-        if (keyH.rightPressed) {
-            playerX += playerSPeed;
-        }
+        player.update();
 
     }
     public void paintComponent(Graphics g) {
@@ -118,9 +110,7 @@ public class GamePanel  extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
 
         g2.dispose();
     }

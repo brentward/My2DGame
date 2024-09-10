0e4b432d0ba2dbf738ea9babb7bd71eb8b15fc9a
Brent Bettis <brent.bettis@gmail.com>
2024-09-10 14:33:30 -0700

    Added world map and camera

--- src/main/java/org/brentwardindustries/entity/Player.java
+++ src/main/java/org/brentwardindustries/entity/Player.java
@@ -4,7 +4,6 @@ import org.brentwardindustries.main.GamePanel;
 import org.brentwardindustries.main.KeyHandler;
 
 import javax.imageio.ImageIO;
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -13,16 +12,23 @@ public class Player extends Entity{
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
+
+        screenX = gp.screenWidth / 2 - (gp.tileSize / 2);
+        screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
+
         setDefaultValues();
         getPlayerImage();
     }
 
     public void setDefaultValues() {
-        x = 100;
-        y = 100;
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
         speed = 4;
         direction = "down";
     }
@@ -46,19 +52,19 @@ public class Player extends Entity{
         if (keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
             if (keyH.upPressed) {
                 direction = "up";
-                y -= speed;
+                worldY -= speed;
             }
             if (keyH.downPressed) {
                 direction = "down";
-                y += speed;
+                worldY += speed;
             }
             if (keyH.leftPressed) {
                 direction = "left";
-                x -= speed;
+                worldX -= speed;
             }
             if (keyH.rightPressed) {
                 direction = "right";
-                x += speed;
+                worldX += speed;
             }
             spriteCounter++;
             if (spriteCounter > 12) {
@@ -108,6 +114,6 @@ public class Player extends Entity{
                 }
             }
         }
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
 }

e4fb6e7b665945f8959b9425a7d0f97496b4b7ca
Brent Bettis <brent.bettis@gmail.com>
2024-09-23 14:10:22 -0700

    Switch Config to use Properties class

--- src/main/java/org/brentwardindustries/main/Config.java
+++ src/main/java/org/brentwardindustries/main/Config.java
@@ -1,82 +1,58 @@
 package org.brentwardindustries.main;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Properties;
 
 public class Config {
     GamePanel gp;
+    final String filePath = "config.txt";
 
     public Config(GamePanel gp) {
         this.gp = gp;
     }
 
     public void saveConfig() {
+        Properties properties = new Properties();
+        properties.setProperty("fullscreen", "" + gp.fullScreenOn);
+        properties.setProperty("music.volume", String.valueOf(gp.music.volumeScale));
+        properties.setProperty("se.volume", String.valueOf(gp.se.volumeScale));
+
         try {
-            String filePath = "config.txt";
-            File file = new File(filePath);
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath));
-            // FULL SCREEN
-            if (gp.fullScreenOn) {
-                bufferedWriter.write("On");
-            } else {
-                bufferedWriter.write("Off");
-            }
-            bufferedWriter.newLine();
-
-            // MUSIC VOLUME
-            bufferedWriter.write(String.valueOf(gp.music.volumeScale));
-            bufferedWriter.newLine();
-
-            // SE VOLUME
-            bufferedWriter.write(String.valueOf(gp.se.volumeScale));
-            bufferedWriter.newLine();
-
-            bufferedWriter.close();
+            FileWriter fileWriter = new FileWriter(filePath);
+            properties.store(fileWriter, "My2DGame config file");
+            fileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void loadConfig()  {
+    public void loadConfig() {
+        Properties properties = new Properties();
         try {
-            String filePath = "config.txt";
             File file = new File(filePath);
-            if (!file.exists()) {
-                file.createNewFile();
+            if (file.exists()) {
+                FileReader fileReader = new FileReader(file);
+                properties.load(fileReader);
+                fileReader.close();
             }
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
-            String string = bufferedReader.readLine();
-
-            // FULL SCREEN
-            if (string != null && string.equals("On")) {
-                gp.fullScreenOn = true;
-            }
-            if (string != null && string.equals("Off")) {
-                gp.fullScreenOn = false;
-            }
-
-            // MUSIC VOLUME
-            string = bufferedReader.readLine();
-            if (string != null) {
-                gp.music.volumeScale = Integer.parseInt(string);
-            }
-
-            // SE VOLUME
-            string = bufferedReader.readLine();
-            if (string != null) {
-                gp.se.volumeScale = Integer.parseInt(string);
-            }
-            bufferedReader.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+        }
+        String string;
+        string = properties.getProperty("fullscreen");
+        if (string != null) {
+            gp.fullScreenOn = string.equals("true");
+        }
+        string = properties.getProperty("music.volume");
+        if (string != null) {
+            gp.music.volumeScale = Integer.parseInt(string);
+        }
+        string = properties.getProperty("se.volume");
+        if (string != null) {
+            gp.se.volumeScale = Integer.parseInt(string);
         }
     }
 }
-
